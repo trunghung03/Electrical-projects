c935c0aff50611aacd2ad7211c6184a4c8e19947
synapse26 <61304518+trunghung03@users.noreply.github.com>
2022-11-11 22:10:03 +0700

    Added cycling through timezones

--- ControlPanel/src.ino
+++ ControlPanel/src.ino
@@ -1,5 +1,5 @@
-#include <U8glib.h> // OLED Display library
-#include <virtuabotixRTC.h> // Clock library
+#include <U8glib.h>          // OLED Display library
+#include <virtuabotixRTC.h>  // Clock library
 
 #include "DHT.h"
 
@@ -12,25 +12,30 @@ DHT dht(DHTPIN, DHTTYPE);
 #define SLOT_WIDTH 20
 #define Y_OFFSET 32
 
-// Timezone selection pins
+// Cycle through timezone button
 #define BUTTON1 9
-#define BUTTON2 10
-#define BUTTON3 11
-#define BUTTON4 12
 
-#define LEDPIN 3          // LED pin
-#define FADESPEED 5      // LED fade speed
+#define LEDPIN 3           // LED pin
+#define FADESPEED 5        // LED fade speed
 #define LIGHTSENSORPIN A0  // Light sensor analog pin
 #define LIGHTTHRESHOLD 50  // Threshold where the light sensor will activate
-#define IRSENSORPIN 5     // IR pin
+#define IRSENSORPIN 5      // IR pin
 
-U8GLIB_SSD1306_128X32 u8g(U8G_I2C_OPT_NONE);	// I2C / TWI 
-virtuabotixRTC myRTC(6, 7, 8); // Create real time clock object
+U8GLIB_SSD1306_128X32 u8g(U8G_I2C_OPT_NONE);  // A4:sda A5:scl
+virtuabotixRTC myRTC(6, 7, 8);                // Create real time clock object, pinput: clk, dat, rst
 
-enum place {HCM, NY, LON, SYD}; // List of selectable timezone
-enum place currentPlace = HCM; // Default timezone
-bool LEDON = false; // Set variable to keep track of LED status
-bool fullBrightness = false; // Keep track of LED brightness
+#define PLACES 4
+enum place { HCM,
+             NY,
+             LON,
+             SYD };                  // List of selectable timezone
+enum place currentPlace = HCM;       // Default timezone
+int buttonState = HIGH;                     // Capture state of timezone input pin
+unsigned long lastDebounceTime = 0;  // Last time timezone pin was toggled
+unsigned long debounceDelay = 50;    // debounce time, increase if output flickers
+
+bool LEDON = false;           // Set variable to keep track of LED status
+bool fullBrightness = false;  // Keep track of LED brightness
 
 inline int positive_modulo(int i, int n) {
   return (i % n + n) % n;
@@ -43,8 +48,7 @@ void fadeLED(boolean turnOn, int fadeSpeed) {
       delay(fadeSpeed);
     }
     fullBrightness = true;
-  }
-  else {
+  } else {
     for (int i = 255; i > -1; i--) {
       analogWrite(LEDPIN, i);
       delay(fadeSpeed);
@@ -54,18 +58,14 @@ void fadeLED(boolean turnOn, int fadeSpeed) {
 }
 
 enum place getPlace() {
-  if (digitalRead(BUTTON1) == LOW) {
-    return HCM;
+  int reading = digitalRead(BUTTON1);
+
+  if (reading == LOW) {
+    if (buttonState == HIGH) {
+      currentPlace = (currentPlace + 1) % 4;
+    }
   }
-  else if (digitalRead(BUTTON2) == LOW) {
-    return NY;
-  }
-  else if (digitalRead(BUTTON3) == LOW) {
-    return LON;
-  }
-  else if (digitalRead(BUTTON4) == LOW) {
-    return SYD;
-  }
+  buttonState = reading;
 }
 
 void draw(void) {
@@ -73,27 +73,27 @@ void draw(void) {
   uint8_t Hrs = myRTC.hours;
 
   // Print place depending on current selection
-  currentPlace = getPlace();
+  getPlace();
 
   switch (currentPlace) {
-    case HCM: 
-    u8g.drawStr(0, 11, "Ho Chi Minh");
-    break;
+    case HCM:
+      u8g.drawStr(0, 11, "Ho Chi Minh");
+      break;
 
-    case NY: 
-    u8g.drawStr(0, 11, "New York");
-    Hrs = positive_modulo(Hrs - 11, 24); //Used a special function to make sure there's no negative modulo
-    break;
+    case NY:
+      u8g.drawStr(0, 11, "New York");
+      Hrs = positive_modulo(Hrs - 11, 24);  //Used a special function to make sure there's no negative modulo
+      break;
 
-    case LON: 
-    u8g.drawStr(0, 11, "London");
-    Hrs = positive_modulo(Hrs - 6, 24);
-    break;
+    case LON:
+      u8g.drawStr(0, 11, "London");
+      Hrs = positive_modulo(Hrs - 6, 24);
+      break;
 
-    case SYD: 
-    u8g.drawStr(0, 11, "Sydney");
-    Hrs = positive_modulo(Hrs + 4, 24);
-    break;
+    case SYD:
+      u8g.drawStr(0, 11, "Sydney");
+      Hrs = positive_modulo(Hrs + 4, 24);
+      break;
   }
 
   // Print temperature
@@ -116,21 +116,14 @@ void draw(void) {
 }
 
 void setup(void) {
+  Serial.begin(9600);
+
   // Set temperature sensor
   dht.begin();
 
   //Set Pull up resistor
   pinMode(BUTTON1, INPUT);
   digitalWrite(BUTTON1, HIGH);
-
-  pinMode(BUTTON2, INPUT);
-  digitalWrite(BUTTON2, HIGH);
-
-  pinMode(BUTTON3, INPUT);
-  digitalWrite(BUTTON3, HIGH);
-
-  pinMode(BUTTON4, INPUT);
-  digitalWrite(BUTTON4, HIGH);
 
   //myRTC.setDS1302Time(0, 21, 13, 6, 14, 10, 2022);
   // seconds, minutes, hours, day of the week, day of the month, month, year
@@ -141,17 +134,14 @@ void setup(void) {
   pinMode(LEDPIN, OUTPUT);
 
   // assign default color value
-  if ( u8g.getMode() == U8G_MODE_R3G3B2 ) {
-    u8g.setColorIndex(255);     // white
-  }
-  else if ( u8g.getMode() == U8G_MODE_GRAY2BIT ) {
-    u8g.setColorIndex(3);         // max intensity
-  }
-  else if ( u8g.getMode() == U8G_MODE_BW ) {
-    u8g.setColorIndex(1);         // pixel on
-  }
-  else if ( u8g.getMode() == U8G_MODE_HICOLOR ) {
-    u8g.setHiColorByRGB(255,255,255);
+  if (u8g.getMode() == U8G_MODE_R3G3B2) {
+    u8g.setColorIndex(255);  // white
+  } else if (u8g.getMode() == U8G_MODE_GRAY2BIT) {
+    u8g.setColorIndex(3);  // max intensity
+  } else if (u8g.getMode() == U8G_MODE_BW) {
+    u8g.setColorIndex(1);  // pixel on
+  } else if (u8g.getMode() == U8G_MODE_HICOLOR) {
+    u8g.setHiColorByRGB(255, 255, 255);
   }
 }
 
@@ -161,7 +151,7 @@ void loop(void) {
   do {
     myRTC.updateTime();
     draw();
-  } while( u8g.nextPage() );
+  } while (u8g.nextPage());
 
   // LED loop
   float LSensor = analogRead(LIGHTSENSORPIN);  // measured in lux
@@ -173,19 +163,18 @@ void loop(void) {
 
   if (LSensor < LIGHTTHRESHOLD && IRSensor == 1) {  // If no light and sensor is on (IR on is 0 for some reasons)
     // LED fades on
-    if (!fullBrightness) { // If light is already on then no fade on
+    if (!fullBrightness) {  // If light is already on then no fade on
       fadeLED(true, FADESPEED);
     }
     LEDON = true;
   } else {
     // LED fades off
-    if (fullBrightness) { // If light is already off then no fade off
+    if (fullBrightness) {  // If light is already off then no fade off
       fadeLED(false, FADESPEED);
     }
     LEDON = false;
   }
-  
-  // rebuild the picture after some delay
-  delay(50); 
-}
 
+  // rebuild the picture after some delay
+  delay(50);
+}
